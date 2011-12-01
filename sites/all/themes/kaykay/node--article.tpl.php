ffceca5beb731111ea4040ec2ce3915c0f16caf5
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2011-12-01 18:54:58 +0100

    Updated events and articles to support carousel

--- sites/all/themes/kaykay/node--article.tpl.php
+++ sites/all/themes/kaykay/node--article.tpl.php
@@ -1,39 +1,84 @@
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<?php if($view_mode=='full'):?>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div class="clearfix content"<?php print $content_attributes; ?>>
+        <h1><?php print $title; ?>  </h1>
+      <?php if($content['body']['#items'][0]['summary']): ?>
+        <div class="summary"><?php print render($content['body']['#items'][0]['summary']); ?></div>
+      <?php endif; ?>
+      <?php if ($display_submitted): ?>
+        <div class="submitted">
+          <?php print $submitted; ?>
+        </div>
+      <?php endif; ?>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_main_image']);
+        print render($content);
+      ?>
+    </div>
 
-  <?php print $user_picture; ?>
+    <?php /*FIXME move to module and print $comments*/if($node->comment==COMMENT_NODE_OPEN):?>
+      <div class="comments-box">
+        <h2><?php print t('Comments');?></h2>
+        <div class="fb-comments" data-num-posts="3"></div>
+      </div>    
+    <?php endif;?>
+  </div>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>">a<?php print $title; ?>b</a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-
-
-
-  <div class="content"<?php print $content_attributes; ?>>
-      <h1><?php print $title; ?>  </h1>
-    <?php if($content['body']['#items'][0]['summary']): ?>
-      <div class="summary"><?php print render($content['body']['#items'][0]['summary']); ?></div>
-    <?php endif; ?>
-    <?php if ($display_submitted): ?>
-      <div class="submitted">
-        <?php print $submitted; ?>
-      </div>
-    <?php endif; ?>
+<?php elseif($view_mode=='large_item'):?>
+  <div class="large-item-wrapper">
+    <h4><?php print l($title,'node/'.$node->nid); ?></h4>
+    
+    <?php print render($content['field_image']);?>
+    <div class="large-item-content <?php if(!isset($content['field_image'])) print "no-image";?>">
+      <div class="property-bar">
+        <?php print format_date($node->created,'article_short');?>
+        <?php if(isset($node->field_location[$node->language][0]['name'])):?>
+          <div class="article-author"><?php print $node->author; ?></div> 
+        <?php endif;?>
+      </div>        
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      hide($content['field_main_image']);
       print render($content);
     ?>
+    </div>
   </div>
 
-  <?php /*FIXME move to module and print $comments*/if($node->comment==COMMENT_NODE_OPEN):?>
-    <div class="comments-box">
-      <h2><?php print t('Comments');?></h2>
-      <div class="fb-comments" data-num-posts="3"></div>
-    </div>    
-  <?php endif;?>
-</div>
+  
+<?php elseif($view_mode=='carousel_item'):?>
+  <div class="carousel-item-wrapper">
+    <?php print render($content['field_image']);?>
+    <div class="carousel-item-content <?php if(!isset($content['field_image'])) print "no-image";?>">
+      <h4><?php print l($title,'node/'.$node->nid); ?></h4>
+      <div class="property-bar">
+        <?php print format_date($node->created,'article_short');?>
+        <?php if(isset($node->field_location[$node->language][0]['name'])):?>
+          <div class="article-author"><?php print $node->author; ?></div> 
+        <?php endif;?>
+      </div>        
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+    </div>
+  </div>
+
+
+
+    
+<?php elseif($view_mode=='list_item'):?>
+  <div class="list-item-wrapper">
+    <div class="list-item-content">
+      <div class="property-bar">
+        <?php print format_date($node->created,'article_short');?>
+      </div>
+    </div>
+    <h4><?php print l($title,'node/'.$node->nid); ?></h4>
+  </div>
+<?php endif; ?>
--- sites/all/themes/kaykay/node--event.tpl.php
+++ sites/all/themes/kaykay/node--event.tpl.php
@@ -1,47 +1,49 @@
-<?php //krumo($node);?>
 <?php if($view_mode=='full'):?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="clearfix content"<?php print $content_attributes; ?>>
-    <h1><?php print $title; ?>  </h1>
-    <?php if($content['body']['#items'][0]['summary']): ?>
-      <div class="summary"><?php print render($content['body']['#items'][0]['summary']); ?></div>
-    <?php endif; ?>
-    <?php if ($display_submitted): ?>
-      <div class="submitted">
-        <?php print $submitted; ?>
-      </div>
-    <?php endif; ?>
-   <?php print render($content['field_image']);?>
-      
-    <div class="event-properties">
-      <div class="event-property event-location">
-        <?php print render($content['field_location']);?>
-      </div>
-      <div class="event-property event-datetime">
-        <?php print render($content['field_event_time']);?>
-      </div>
-      <?php if(isset($content['field_email'])||isset($content['field_website'])):?>
-      <div class="event-property event-extras">
-        <?php print render($content['field_email']);?>
-        <?php print render($content['field_links']);?>
-      </div>
-      <?php endif;?>
-    </div>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div class="clearfix content"<?php print $content_attributes; ?>>
+      <h1><?php print $title; ?>  </h1>
+      <?php if($content['body']['#items'][0]['summary']): ?>
+        <div class="summary"><?php print render($content['body']['#items'][0]['summary']); ?></div>
+      <?php endif; ?>
+      <?php if ($display_submitted): ?>
+        <div class="submitted">
+          <?php print $submitted; ?>
+        </div>
+      <?php endif; ?>
+     <?php print render($content['field_image']);?>
 
-  <?php /*FIXME move to module and print $comments*/if($node->comment==COMMENT_NODE_OPEN):?>
-    <div class="comments-box">
-      <h2><?php print t('Comments');?></h2>
-      <div class="fb-comments" data-num-posts="3"></div>
-    </div>    
-  <?php endif;?>
-</div>
+      <div class="event-properties">
+        <div class="event-property event-category">
+          <?php print render($content['field_event_category']);?>
+        </div>
+        <div class="event-property event-location">
+          <?php print render($content['field_location']);?>
+        </div>
+        <div class="event-property event-datetime">
+          <?php print render($content['field_event_time']);?>
+        </div>
+        <?php if(isset($content['field_email'])||isset($content['field_website'])):?>
+        <div class="event-property event-extras">
+          <?php print render($content['field_email']);?>
+          <?php print render($content['field_links']);?>
+        </div>
+        <?php endif;?>
+      </div>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+      ?>
+    </div>
+
+    <?php /*FIXME move to module and print $comments*/if($node->comment==COMMENT_NODE_OPEN):?>
+      <div class="comments-box">
+        <h2><?php print t('Comments');?></h2>
+        <div class="fb-comments" data-num-posts="3"></div>
+      </div>    
+    <?php endif;?>
+  </div>
 
 
 
@@ -57,6 +59,7 @@
         <?php if(isset($node->field_location[$node->language][0]['name'])):?>
           <div class="event-location-name"><?php print $node->field_location[$node->language][0]['name']; ?></div> 
         <?php endif;?>
+        <?php print render($content['field_event_category']);?>
       </div>        
     <?php
       // We hide the comments and links now so that we can render them later.
@@ -66,6 +69,52 @@
     ?>
     </div>
   </div>
+
+  
+<?php elseif($view_mode=='carousel_item'):?>
+  <div class="carousel-item-wrapper">
+    <?php print render($content['field_image']);?>
+    <div class="carousel-item-content <?php if(!isset($content['field_image'])) print "no-image";?>">
+      <h4><?php print l($title,'node/'.$node->nid); ?></h4>
+      <div class="property-bar">
+        <?php print render($content['field_event_time']);?>
+        <?php if(isset($node->field_location[$node->language][0]['name'])):?>
+          <div class="event-location-name"><?php print $node->field_location[$node->language][0]['name']; ?></div> 
+        <?php endif;?>
+        <?php print render($content['field_event_category']);?>
+      </div>        
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+    </div>
+  </div>
+
+
+<?php elseif($view_mode=='imagetile_item'):?>
+  <div class="imagetile-item-wrapper">
+    <h4><?php print l($title,'node/'.$node->nid); ?></h4>
+    
+    <?php print render($content['field_image']);?>
+    <div class="imagetile-item-content <?php if(!isset($content['field_image'])) print "no-image";?>">
+      <div class="property-bar">
+        <?php print render($content['field_event_time']);?>
+        <?php if(isset($node->field_location[$node->language][0]['name'])):?>
+          <div class="event-location-name"><?php print $node->field_location[$node->language][0]['name']; ?></div> 
+        <?php endif;?>
+        <?php print render($content['field_event_category']);?>          
+      </div>        
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+    </div>
+  </div>
+    
     
     
 <?php elseif($view_mode=='list_item'):?>
