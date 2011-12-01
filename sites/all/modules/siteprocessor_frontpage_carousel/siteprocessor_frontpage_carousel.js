39a4452dcaaffaf1699a670f7d676d380a47105d
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2011-12-01 18:56:36 +0100

    Made a frontage carousel of images and articles based on node queue

---
+++ sites/all/modules/siteprocessor_frontpage_carousel/siteprocessor_frontpage_carousel.js
@@ -0,0 +1,9 @@
+jQuery().ready(function(){
+  jQuery('.carousel-view .view-content .item-list').jCarouselLite({
+    auto: 3000,
+    speed: 500,
+    visible:1,
+    circular:true,
+    hoverPause:true
+  });
+});
