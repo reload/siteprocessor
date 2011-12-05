3d869d0788a4219ff59050baf4d8a6bf50a6e7f8
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2011-12-05 17:07:11 +0100

    Added new layout 100% width single column

---
+++ sites/all/themes/kaykay/layouts/100x/100x.tpl.php
@@ -0,0 +1,7 @@
+<div class="panel-display panel-100 clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="center-wrapper panel-row panel-row-1 panel-row-first clearfix">
+    <div class="panel-panel panel-col panel-col-last panel-col-first">
+      <div class="inside"><?php print $content['1']; ?></div>
+    </div>
+  </div>
+</div>
