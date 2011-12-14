523c4188b6c18206cbc7c7a3d8349f397a39280a
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2011-12-14 10:58:40 +0100

    Updated panel layouts

--- sites/all/themes/kaykay/layouts/100-66-33/100-66-33.tpl.php
+++ sites/all/themes/kaykay/layouts/100-66-33/100-66-33.tpl.php
@@ -1,17 +1,17 @@
 <div class="panel-display panel-100-66-33 clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="toparea-wrapper">
     <div class="center-wrapper panel-row panel-row-1 panel-row-first clearfix">
-      <div class="panel-panel panel-col panel-col-last panel-col-first">
+      <div class="panel-panel panel-col panel-col-last panel-col-first panel-col-1-1">
         <div class="inside"><?php print $content['1']; ?></div>
       </div>
     </div>
   </div>
   <div class="content-wrapper">
     <div class="center-wrapper panel-row panel-row-2 panel-row-last panel-row-split clearfix">
-      <div class="panel-panel panel-col panel-col-first panel-col-maincontent">
+      <div class="panel-panel panel-col panel-col-first panel-col-maincontent panel-col-2-3">
         <div class="inside"><?php print $content['2_1']; ?></div>
       </div>
-      <div class="panel-panel panel-col panel-col-last panel-col-right panel-sidebar">
+      <div class="panel-panel panel-col panel-col-last panel-col-right panel-sidebar  panel-col-1-3">
         <div class="inside"><?php print $content['2_2']; ?></div>
       </div>
     </div>
