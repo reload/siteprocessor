e66b144f172185effeac98ec02860e6f245c1a8f
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2011-12-30 14:47:30 +0100

    Fixed issues with heights of image tile heights in IE7

--- themes/kaykay/js/kaykay.js
+++ themes/kaykay/js/kaykay.js
@@ -1,12 +1,35 @@
+var fixTileHeights=function(){
+  var maxHeight=0;
+  var lastLine=[];
+  var lastYPos=0;
+  var yPos=0;
+  jQuery('.imagetile-item').each(function(){
+    yPos=jQuery(this).offset().top;
+    if((yPos-lastYPos)>1 && lastYPos>0) {
+      for(var i in lastLine){
+        maxHeight=Math.max(maxHeight,jQuery(lastLine[i]).height());
+      }
+      for(var i in lastLine){
+        jQuery(lastLine[i]).height(maxHeight);
+      }
+      lastLine=[];
+      maxHeight=0;
+    }
+    lastYPos=yPos;
+    lastLine.push(this);
+  });  
+}
+
 jQuery(document).ready(function() {
   jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+1)').css('clear','left');
   jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+1)').css('margin-right','22px');
   jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+2)').css('margin-right','22px');
-
+  fixTileHeights();
   jQuery('.pane-views-panes').ajaxStop(function(){
     jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+1)').css('clear','left');
     jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+1)').css('margin-right','22px');
     jQuery('.panel-col-1-1 li.imagetile-item:nth-child(3n+2)').css('margin-right','22px');
+    fixTileHeights();
   });
 });
 
