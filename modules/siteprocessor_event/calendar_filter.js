2d546c05777f37f8443c34a1941d1678534f0199
Martin Elvar <m@martinelvar.dk>
2012-04-27 12:57:24 +0200

    Trigger change on select instead

--- modules/siteprocessor_event/calendar_filter.js
+++ modules/siteprocessor_event/calendar_filter.js
@@ -6,6 +6,6 @@ jQuery(function($){
 
     $('input[name="field_event_time_value[min][date]"]').val(splittetLink[splittetLink.length -1]);
     $('input[name="field_event_time_value[max][date]"]').val(splittetLink[splittetLink.length -1]);
-    $('input[name="field_event_time_value[max][date]"]').change();
+    $('.view-id-search_for_events_by_calendar select').trigger('change');
   });
 });
