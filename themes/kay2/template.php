fe91758baacb4f566aec85a52f988adf9e57e502
Hasse R. Hansen <hasse@ramlev.dk>
2013-01-18 15:17:41 +0100

    Change the full node field_image style preset

--- themes/kay2/template.php
+++ themes/kay2/template.php
@@ -5,13 +5,22 @@
  */
 function kay2_preprocess_node(&$vars) {
   $vars['language_fields'] = field_language('node', $vars['node']);
+
+  // Need to overwrite field_image style preset for only this theme.
+  if ($vars['elements']['#view_mode'] == 'full') {
+    if (isset($vars['content']['field_image'])) {
+      $vars['content']['field_image'][0]['#item']['width'] = '250';
+      $vars['content']['field_image'][0]['#item']['height'] = 'auto';
+      $vars['content']['field_image'][0]['#image_style'] = 'node_image';
+    }
+  }
 }
+
 
 /**
  * Implements hook_process_html().
  */
 function kay2_process_html(&$variables) {
-
   // Hook into color.module.
   if (module_exists('color')) {
     _color_html_alter($variables);
@@ -25,5 +34,5 @@ function kay2_process_page(&$variables) {
   // Hook into color.module.
   if (module_exists('color')) {
     _color_page_alter($variables);
-}
+  }
 }
