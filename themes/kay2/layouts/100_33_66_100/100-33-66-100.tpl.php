8496f9c5541e4a8bd39a8cce05a16d1365369dc9
Hasse R. Hansen <hasse@ramlev.dk>
2013-11-26 08:39:01 +0100

    Created new stacked two column panel layout

---
+++ themes/kay2/layouts/100_33_66_100/100-33-66-100.tpl.php
@@ -0,0 +1,26 @@
+<div class="panel-100-33-66-100 row" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="region top span12">
+    <div class="inner">
+      <?php print $content['1']; ?>
+    </div>
+  </div>
+
+  <div class="region left span4">
+    <div class="inner">
+      <?php print $content['2_1']; ?>
+    </div>
+  </div>
+
+  <div class="region main span8">
+    <div class="inner">
+      <?php print $content['2_2']; ?>
+    </div>
+  </div>
+
+  <div class="region bottom span12">
+    <div class="inner">
+      <?php print $content['3']; ?>
+    </div>
+  </div>
+</div>
+
